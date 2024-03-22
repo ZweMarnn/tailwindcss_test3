3aa6d70b5c2b9189225006f0be96c529e0fd12d7
ZweMarnn <zwenyimarnn2001@gmail.com>
2024-03-22 12:03:45 +0630

    add all files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      'smoochSans':["Smooch Sans", 'sans-serif']
+    },
+    colors:{
+      'padauk':'#fcba03',
+      'kyae_ne':'#b5640d',
+      'sein_pyar':'#068f8d'
+    }
+  },
+  plugins: [
+    
+  ],
+}
